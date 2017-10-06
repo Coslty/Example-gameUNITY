566602178c2a022380da8c2f9411f62daff16156
Coslty <kengyin123@gmail.com>
2017-10-06 11:17:42 -0800

    E7 Done!

--- Game Tutorial 2D/Assets/playerController.cs
+++ Game Tutorial 2D/Assets/playerController.cs
@@ -10,6 +10,12 @@ public class playerController : MonoBehaviour {
     Animator myAnim;
     bool facingRight;
 
+    //jumping variables
+    bool grounded = false;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
 
 
 
@@ -19,9 +25,33 @@ public class playerController : MonoBehaviour {
         myAnim = GetComponent<Animator>();
         facingRight = true;
 	}
+
 	
 	// Update is called once per frame
+    void Update()
+    {
+        if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            myAnim.SetBool("isGrounded", grounded);
+            myRB.AddForce(new Vector2(0, jumpHeight));
+        }
+    }
+
+
+
+
+
+
+
+
 	void FixedUpdate () {
+        //check if we are grounded - if no, then we are falling
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        myAnim.SetBool("isGrounded", grounded);
+
+        myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
+
         float move = Input.GetAxis("Horizontal");
         myAnim.SetFloat("speed", Mathf.Abs(move));
 
